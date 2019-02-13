bc0e105285eacd0617d15c93ec586e8f98a76654
Jeff DeLiso <jeff.deliso@gmail.com>
2019-02-13 12:05:06 -0500

    Fix small error

--- lib/scaffold/app/controllers/bands_controller.rb
+++ lib/scaffold/app/controllers/bands_controller.rb
@@ -22,7 +22,7 @@ class BandsController < ApplicationController
     if @band.save
       redirect_to band_url(@band)
     else
-      flash.now[:errors] = @band.errors.full_messages
+      flash.now[:errors] = @band.errors
       render :new
     end
   end
@@ -37,7 +37,7 @@ class BandsController < ApplicationController
     if @band.update_attributes(band_params)
       redirect_to band_url(@band)
     else
-      flash.now[:errors] = @band.errors.full_messages
+      flash.now[:errors] = @band.errors
       render :edit
     end
   end
